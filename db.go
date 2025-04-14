6ae8c07ffa0c8ed81d899898676b1ea0be16e4eb
James <KeeperSoBe@gmail.com>
2025-04-14 17:54:44 +0100

    feat: implement mongodb connection, mock return points

---
+++ db.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+type PointCoordinate struct {
+	ID uint `json:"id"`
+	Coordinates [2]float32 `json:"coordinates"`
+}
+
+type Point struct {
+	ID string `json:"id"`
+	Type string `json:"type"`
+	Radius uint `json:"radius"`
+	Coordinates [2]float32 `json:"coordinates"`
+}
+
+type LineString struct {
+	ID string `json:"id"`
+	Type string `json:"type"`
+	LineWidth uint `json:"lineWidth"`
+	Coordinates []PointCoordinate `json:"coordinates"`
+}
+
+type Polygon struct {
+	ID string `json:"id"`
+	Type string `json:"type"`
+	LineWidth uint `json:"lineWidth"`
+	Coordinates []PointCoordinate `json:"coordinates"`
+}
+
+
+func createPoint() Point {
+	return Point{
+		ID: "mock-uuid",
+		Type: "Point",
+		Radius: 1,
+		Coordinates: [2]float32{1, 2},
+	}
+}
+
+var geoFences = []Point{
+	createPoint(),
+}
+
+func FetchGeoFences(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, geoFences)
+}
+
+// Validate the database env vars and return them.
+func getDbVars() (
+	uri string,
+	dbName string,
+	dbUser string,
+	dbPass string,
+	) {
+	uri = os.Getenv("DB_HOST")
+	dbName = os.Getenv("DB_NAME")
+	dbUser = os.Getenv("DB_USER")
+	dbPass = os.Getenv("DB_PASS")
+
+	var valid = true
+
+	if len(uri) == 0 {
+		log.Fatal("You must set your 'DB_HOST' environment variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/")
+		valid = false
+	} else if len(dbName) == 0 {
+		log.Fatal("You must set your 'DB_NAME' environment variable")
+		valid = false
+	} else if len(dbUser) == 0 {
+		log.Fatal("You must set your 'DB_USER' environment variable")
+		valid = false
+	} else if len(dbPass) == 0 {
+		log.Fatal("You must set your 'DB_PASS' environment variable")
+		valid = false
+	}
+
+	if !valid {
+		os.Exit(1)
+	}
+
+	return uri, dbName, dbUser, dbPass
+}
+
+// Setup and confirm the database connection.
+func SetupDB() {
+	uri,
+	dbName,
+	dbUser,
+	dbPass := getDbVars();
+
+	credential := options.Credential{
+		Username: dbUser,
+		Password: dbPass,
+	}
+
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1.
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	// Set the Mongo credentials.
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI).SetAuth(credential)
+	// Create a new client and connect to the server.
+	client, err := mongo.Connect(opts)
+	
+	if err != nil {
+		panic(err)
+	}
+	
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Send a ping to confirm a successful connection.
+	var result bson.M
+
+	if err := client.Database(dbName).RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Decode(&result); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+}
--- main.go
+++ main.go
@@ -3,38 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
-type Point struct {
-	ID string `json:"id"`
-	Type string `json:"type"`
-	Radius uint `json:"radius"`
-	Coordinates [2]float32 `json:"coordinates"`
-}
-
-func createPoint() Point {
-	return Point{
-		ID: "mock-uuid",
-		Type: "Point",
-		Radius: 1,
-		Coordinates: [2]float32{1, 2},
-	}
-}
-
-var geoFences = []Point{
-	createPoint(),
-}
-
-func fetchGeoFences(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, geoFences)
-}
-
+// Entry point of the program.
 func main() {
+	// Initialise dotenv.
 	if godotenv.Load() != nil {
 		log.Fatal("Error loading .env file")
 		os.Exit(1)
@@ -42,8 +19,11 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/", fetchGeoFences)
-
+	router.GET("/", FetchGeoFences)
+	router.GET("/:id", FetchGeoFences)
+	router.POST("/", FetchGeoFences)
+	router.PATCH("/:id", FetchGeoFences)
+	router.DELETE("/:id", FetchGeoFences)
 
 	var port = os.Getenv("PORT");
 
